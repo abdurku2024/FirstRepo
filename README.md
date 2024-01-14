c348f996d94d884c9beaa70a86f895012d954230
abdurku2024 <abdurku2003@gmail.com>
2024-01-14 21:15:45 +0600

    new file added and code updated

---
+++ README.md
@@ -0,0 +1,6 @@
+# First repo
+## C++ programming languge
+- algorithm 
+- git add . 
+- git commit -m "comment"
+- git push 
--- array.cpp
+++ array.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 //#include<bits/stdc++.h>
 using namespace std;
+// code executin part start
 int main()
 {
     int arr[5]={0},i=0;
